e18b88274d404e3913f6d237daecfb00fd90dbbc
JJeferson <48735241+JJeferson@users.noreply.github.com>
2021-09-19 12:04:41 -0300

    commit 0

---
+++ src/main/java/com/hexagonaloverview/HexagonaloverviewApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonaloverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonaloverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonaloverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonaloverview/HexagonaloverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonaloverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonaloverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
